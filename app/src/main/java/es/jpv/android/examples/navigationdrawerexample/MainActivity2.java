0aa3252ff069d133a89a98e117b34d0ad0b5cec7
unknown <0010120@S4D29345.insags.com>
2015-07-14 14:55:24 +0200

    Initial commit

---
+++ app/src/main/java/es/jpv/android/examples/navigationdrawerexample/MainActivity2.java
@@ -0,0 +1,139 @@
+package es.jpv.android.examples.navigationdrawerexample;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity2 extends AppCompatActivity
+        implements
+        MyAdapter.ViewHolder.OnNavDrwItemClickListener,
+        PlaceholderFragment.OnFragmentInteractionListener{
+
+    //First We Declare Titles And Icons For Our Navigation Drawer List View
+    //This Icons And Titles Are holded in an Array as you can see
+
+    String TITLES[] = {"Home","DNA","Fish","Cats","People"};
+    int ICONS[] = {R.drawable.ic_action_home,R.drawable.ic_action_dna,R.drawable.ic_action_fish,R.drawable.ic_action_cat,R.drawable.ic_action_people};
+
+    //Similarly we Create a String Resource for the name and email in the header view
+    //And we also create a int resource for profile picture in the header view
+
+    String NAME = "Me, I and Myself";
+    String EMAIL = "myself@gmail.com";
+
+    private Toolbar toolbar;                              // Declaring the Toolbar Object
+
+    RecyclerView mRecyclerView;                           // Declaring RecyclerView
+    RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+    DrawerLayout drawer;                                  // Declaring DrawerLayout
+
+    ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView); // Assigning the RecyclerView Object to the xml View
+        mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+        mAdapter = new MyAdapter(TITLES, ICONS, NAME, EMAIL, this);     // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
+        // And passing the titles,icons,header view name, header view email,
+        // and header view profile picture
+        mRecyclerView.setAdapter(mAdapter);                             // Setting the adapter to RecyclerView
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                //Do nothing afterwards
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //Do nothing afterwards
+            }
+        };
+
+        drawer.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(
+                    R.id.fragmentContainer,
+                    PlaceholderFragment.newInstance(1),
+                    "navDrwrContentFragment").commit();
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(
+                    R.id.fragmentContainer,
+                    getSupportFragmentManager().findFragmentByTag("navDrwrContentFragment"),
+                    "navDrwrContentFragment").commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onNavDrwItemClick(View view) {
+        int position = (int) view.getTag(R.string.navDrwrPosition);
+        switch (position) {
+            case 1:
+                Toast.makeText(this, "Fragment Inicio", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(
+                        R.id.fragmentContainer,
+                        PlaceholderFragment.newInstance(position),
+                        "navDrwrContentFragment").commit();
+                break;
+            default:
+                getSupportFragmentManager().beginTransaction().replace(
+                        R.id.fragmentContainer,
+                        PlaceholderFragment.newInstance(position),
+                        "navDrwrContentFragment").commit();
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
